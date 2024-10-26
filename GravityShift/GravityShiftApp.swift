c96190a4bb8915cdf56426dd9cea31baa4cde668
Munish Mummadi <moneymindedmunish1@gmail.com>
2024-10-25 20:24:12 -0500

    Initial Commit

---
+++ GravityShift/GravityShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GravityShiftApp.swift
+//  GravityShift
+//
+//  Created by Munish Mummadi on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GravityShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GravityShiftTests/GravityShiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  GravityShiftTests.swift
+//  GravityShiftTests
+//
+//  Created by Munish Mummadi on 10/25/24.
+//
+
+import Testing
+@testable import GravityShift
+
+struct GravityShiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
